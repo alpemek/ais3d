15b675c48b1af1dfce9abd072e670aff2297e9fd
Marcelo Chulek <marcelo.chulek@gmail.com>
2018-05-30 12:34:48 +0200

    Updated the names

--- ssd.pytorch-master/demo/demo.py
+++ ssd.pytorch-master/demo/demo.py
@@ -23,49 +23,50 @@ from matplotlib import pyplot as plt
 from data import VOCDetection, VOC_ROOT, VOCAnnotationTransform
 # here we specify year (07 or 12) and dataset ('test', 'val', 'train')
 testset = VOCDetection(VOC_ROOT, [('2007', 'val')], None, VOCAnnotationTransform())
-img_id = 60
-image = testset.pull_image(img_id)
-rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-# View the sampled input image before transform
-plt.figure(figsize=(10,10))
-plt.imshow(rgb_image)
-plt.show()
 
-x = cv2.resize(image, (300, 300)).astype(np.float32)
-x -= (104.0, 117.0, 123.0)
-x = x.astype(np.float32)
-x = x[:, :, ::-1].copy()
-plt.imshow(x)
-plt.show()
-x = torch.from_numpy(x).permute(2, 0, 1)
+for img_id in range(10):
+    image = testset.pull_image(img_id)
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # View the sampled input image before transform
+    plt.figure(figsize=(10,10))
+    plt.imshow(rgb_image)
+    #plt.show()
 
-xx = Variable(x.unsqueeze(0))     # wrap tensor in Variable
-if torch.cuda.is_available():
-    xx = xx.cuda()
-y = net(xx)
+    x = cv2.resize(image, (300, 300)).astype(np.float32)
+    x -= (104.0, 117.0, 123.0)
+    x = x.astype(np.float32)
+    x = x[:, :, ::-1].copy()
+    plt.imshow(x)
+    #plt.show()
+    x = torch.from_numpy(x).permute(2, 0, 1)
 
-from data import VOC_CLASSES as labels
-top_k=10
+    xx = Variable(x.unsqueeze(0))     # wrap tensor in Variable
+    if torch.cuda.is_available():
+        xx = xx.cuda()
+    y = net(xx)
 
-plt.figure(figsize=(10,10))
-colors = plt.cm.hsv(np.linspace(0, 1, 21)).tolist()
-plt.imshow(rgb_image)  # plot the image for matplotlib
+    from data import VOC_CLASSES as labels
+    top_k=10
 
-currentAxis = plt.gca()
+    plt.figure(figsize=(10,10))
+    colors = plt.cm.hsv(np.linspace(0, 1, 21)).tolist()
+    plt.imshow(rgb_image)  # plot the image for matplotlib
 
-detections = y.data
-# scale each detection back up to the image
-scale = torch.Tensor(rgb_image.shape[1::-1]).repeat(2)
-for i in range(detections.size(1)):
-    j = 0
-    while detections[0,i,j,0] >= 0.6:
-        score = detections[0,i,j,0]
-        label_name = labels[i-1]
-        display_txt = '%s: %.2f'%(label_name, score)
-        pt = (detections[0,i,j,1:]*scale).cpu().numpy()
-        coords = (pt[0], pt[1]), pt[2]-pt[0]+1, pt[3]-pt[1]+1
-        color = colors[i]
-        currentAxis.add_patch(plt.Rectangle(*coords, fill=False, edgecolor=color, linewidth=2))
-        currentAxis.text(pt[0], pt[1], display_txt, bbox={'facecolor':color, 'alpha':0.5})
-        j+=1
-plt.show()
+    currentAxis = plt.gca()
+
+    detections = y.data
+    # scale each detection back up to the image
+    scale = torch.Tensor(rgb_image.shape[1::-1]).repeat(2)
+    for i in range(detections.size(1)):
+        j = 0
+        while detections[0,i,j,0] >= 0.6:
+            score = detections[0,i,j,0]
+            label_name = labels[i-1]
+            display_txt = '%s: %.2f'%(label_name, score)
+            pt = (detections[0,i,j,1:]*scale).cpu().numpy()
+            coords = (pt[0], pt[1]), pt[2]-pt[0]+1, pt[3]-pt[1]+1
+            color = colors[i]
+            currentAxis.add_patch(plt.Rectangle(*coords, fill=False, edgecolor=color, linewidth=2))
+            currentAxis.text(pt[0], pt[1], display_txt, bbox={'facecolor':color, 'alpha':0.5})
+            j+=1
+    plt.show()
