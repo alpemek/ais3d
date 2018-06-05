ad78d00fedc90c64fbd4e9a1fea7b4c63be4caa8
Aemek <emekalp@gmail.com>
2018-06-05 13:08:39 +0200

    working version

--- ssd.pytorch-master/data/voc0712.py
+++ ssd.pytorch-master/data/voc0712.py
@@ -1,9 +1,7 @@
 """VOC Dataset Classes
 
-Original author: Francisco Massa
 https://github.com/fmassa/vision/blob/voc_dataset/torchvision/datasets/voc.py
 
-Updated by: Ellis Brown, Max deGroot
 """
 from .config import HOME
 import os.path as osp
