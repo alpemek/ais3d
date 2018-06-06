ae73062444d767a9621d4406988ea4bfcd15c949
Alp Emek <emekalp@gmail.com>
2018-06-06 14:15:08 +0200

    working version

--- ssd.pytorch-master/demo/live.py
+++ ssd.pytorch-master/demo/live.py
@@ -5,11 +5,12 @@ import cv2
 import time
 from imutils.video import FPS, WebcamVideoStream, FileVideoStream
 import argparse
+import numpy as np
 
 parser = argparse.ArgumentParser(description='Single Shot MultiBox Detection')
-parser.add_argument('--weights', default='/home/emeka/Schreibtisch/AIS/ais3d/ssd.pytorch-master/weights/ssd300_KITTI_75000.pth',
+parser.add_argument('--weights', default='/home/emeka/Schreibtisch/AIS/ais3d/ssd.pytorch-master/weights/ssd300_KITTI_110000.pth',
                     type=str, help='Trained state_dict file path')
-parser.add_argument('--cuda', default=False, type=bool,
+parser.add_argument('--cuda', default=True, type=bool,
                     help='Use cuda in live demo')
 args = parser.parse_args()
 
@@ -19,10 +20,22 @@ FONT = cv2.FONT_HERSHEY_SIMPLEX
 def cv2_demo(net, transform):
     def predict(frame):
         height, width = frame.shape[:2]
-        x = torch.from_numpy(transform(frame)[0]).permute(2, 0, 1)
+        x = cv2.resize(frame, (300, 300)).astype(np.float32) #
+        x -= (104.0, 117.0, 123.0)#
+        x = x.astype(np.float32)#
+        x = x[:, :, ::-1].copy()#
+        #print('Size of x: {}'.format(x.shape))
+        #x = torch.from_numpy(transform(frame)[0]).permute(2, 0, 1)
+        x = torch.from_numpy(x).permute(2, 0, 1) #frame -> x
         x = Variable(x.unsqueeze(0))
+        t = time.time()
+        if torch.cuda.is_available():
+            x = x.cuda()
+
         y = net(x)  # forward pass
         detections = y.data
+        elapsed = time.time() - t
+        print ('Forward pass time: {}'.format(elapsed))
         # scale each detection back up to the image
         scale = torch.Tensor([width, height, width, height])
         for i in range(detections.size(1)):
@@ -49,13 +62,17 @@ def cv2_demo(net, transform):
     while True:
         # grab next frame
         #this for loop was inserted in order to try to make the live in real time (which is currently not happening)
-        for i in range(12):
+        for i in range(5):
             frame = stream.read()
             key = cv2.waitKey(1) & 0xFF
             # update FPS counter
             fps.update()
-        frame = predict(frame)
-
+        t = time.time()
+        if 1:#ind1 ==5:
+            frames = predict(frame)
+            print ('Frame Size: {}'.format(frame.shape))
+            elapsed = time.time() - t
+            print (elapsed)
         # keybindings for display
         if key == ord('p'):  # pause
             while True:
@@ -76,11 +93,12 @@ if __name__ == '__main__':
     from ssd import build_ssd
 
     net = build_ssd('test', 300, 4)    # initialize SSD
-    net.load_state_dict(torch.load(args.weights))
+    #net.load_state_dict(torch.load(args.weights))
+    net.load_weights('../weights/ssd300_KITTI_110000.pth')
     transform = BaseTransform(net.size, (104/2048.0, 117/2048.0, 123/2048.0))
-
+    net.cuda()
     fps = FPS().start()
-    cv2_demo(net.eval(), transform)
+    cv2_demo(net, transform) #.eval()
     # stop the timer and display FPS information
     fps.stop()
 
